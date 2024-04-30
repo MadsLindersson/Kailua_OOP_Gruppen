0194fa9c231af370981bce2f3d9554063f45d270
MadsLindersson <Mali0002@stud.kea.dk>
2024-04-30 13:04:17 +0200

    Worked on Print class, not finished, problem with calling returnString method in subclass.

--- src/PrivateCustomerSub.java
+++ src/PrivateCustomerSub.java
@@ -18,4 +18,32 @@ public class PrivateCustomerSub extends Customer {
 
     public void setDriverSinceDate(String driverSinceDate) {this.driverSinceDate = driverSinceDate;}
 
+    public String returnString()    {
+        StringBuilder niceString = new StringBuilder();
+
+        niceString.append("Name:          " + getName() + "\n");
+        niceString.append("Address:       " + getAddress() + "\n");
+        niceString.append("Postal code:   " + getPostNumber() + "\n");
+        niceString.append("City:          " + getCity() + "\n");
+        niceString.append("Mobile number: " + getMobilePhone() + "\n");
+        niceString.append("Email:         " + getEmail() + "\n");
+        niceString.append("Licensenumber: " + getLicenseNumber() + "\n");
+        niceString.append("Driver since:  " + getDriverSinceDate() + "\n");
+        niceString.append("--------------------------------------------" + "\n");
+
+        return niceString.toString();
+
+       /* for (Customer customer : Main.customers) {
+            System.out.println("Name: " + customer.getName());
+            System.out.println("Address: " + customer.getAddress());
+            System.out.println("Post number: " + customer.getPostNumber());
+            System.out.println("City: " + customer.getCity());
+            System.out.println("Mobile phone: " + customer.getMobilePhone());
+            System.out.println("Email: " + customer.getEmail());
+            System.out.println("Licensenumber: " + );
+            System.out.println("Drivers since: " + );
+            System.out.println("--------------------------------------------");
+            System.out.println();
+        }*/
+    }
 }
